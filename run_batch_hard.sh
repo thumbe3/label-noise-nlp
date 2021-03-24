dc3736a8f93cdf4b232cbc4068c3d901c0598150
Siddhant Garg <sid7954@gmail.com>
2021-03-24 00:34:12 -0700

    Update and rename run_batch.sh to run_batch_hard.sh

--- run_batch_hard.sh
+++ run_batch_hard.sh
@@ -11,13 +11,6 @@ for beta in "${betas[@]}"; do
 python add_noise.py --noise $noise --dataset trec --num_classes 6 --mode 3
 python train_classifier.py --noise $noise --cnn --dataset data/trec --warmup $warmup --beta $beta --max_epoch 100 --round_prob 1 --result cnn/results_length_noise
 
-
-
 done
 done
 done
-
-# 0.1 0.5 0 10-- 92.4, 80.6
-# increase in classes in dataset leads to better performance of our model is one interpretation
-
-
